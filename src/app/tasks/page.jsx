751e192360718840a97b0466fe6d9df905f803a3
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-04-30 17:18:14 +0600

    did theme toggling and added modal

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -1,9 +1,19 @@
+import { AddTask } from "@/components/AddTask";
+import TasksCard from "@/components/TasksCard";
+import { getTasks } from "@/lib/tasks";
 import React from "react";
 
-const TasksPage = () => {
+const TasksPage = async () => {
+  const tasks = await getTasks();
   return (
     <div>
-      <h2>Tasks: 0</h2>
+      <h2>Tasks: {tasks.length}</h2>
+      <AddTask></AddTask>
+      <div className="grid grid-cols-3 gap-4 pt-8">
+        {tasks.map((task) => (
+          <TasksCard key={task.id} task={task}></TasksCard>
+        ))}
+      </div>
     </div>
   );
 };
