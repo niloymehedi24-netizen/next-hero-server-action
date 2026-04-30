dd0bf44998e5f605c05a619e79f42b7d47a1b9ad
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-04-30 15:41:03 +0600

    first commit

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@heroui/react";
+import React from "react";
+
+const HeroPage = () => {
+  return (
+    <div>
+      <h2>Hero Page</h2>
+      <Button>Hero Button</Button>
+      <Button variant="tertiary">Hero Button</Button>
+      <Button variant="danger">Hero Button</Button>
+    </div>
+  );
+};
+
+export default HeroPage;
